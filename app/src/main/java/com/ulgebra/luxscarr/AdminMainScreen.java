6775bc8ea980b85df76ad8ecb5d0403a7a6dbc53
yesiamvj <vijirobotic@gmail.com>
2016-07-07 17:25:40 +0530

    commited

---
+++ app/src/main/java/com/ulgebra/luxscarr/AdminMainScreen.java
@@ -0,0 +1,13 @@
+package com.ulgebra.luxscarr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdminMainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_main_screen);
+    }
+}
